016f487fe8a46e2fbd37146e3c5d3880a1bda76a
Swetha Sivasankararaj <110710815+Swetha5021@users.noreply.github.com>
2024-01-20 20:00:30 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { CrispProvider } from '@/components/crisp-provider'
- 
+
 
 const inter = Inter({ subsets: ['latin'] })
 
